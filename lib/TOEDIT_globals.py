26373d066929d52f361db114e071e8a2833cb4b9
Alberto Becerra <alberto.becerra@quant.global>
2021-10-24 20:50:26 +0200

    globals to edit

---
+++ lib/TOEDIT_globals.py
@@ -0,0 +1,6 @@
+# Script to define global variables
+FISEVI_URL = 'http://fisevi.com/investigador-ofertas-de-empleo/'
+OUTPUT = 'output'
+CRED_PATH = 'security/credentials.txt'
+SMTP_CLIENT = 'smtp.office365.com' # This is the one for Outlook
+RECEIVERS = ['user1@gmail.com', 'user2@gmail.com', 'user3@gmail.com']
