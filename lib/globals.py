5b455c11558415d3620b5ab4488bf25f82c07552
Alberto Becerra <alberto.becerra@quant.global>
2021-10-24 20:46:00 +0200

    delete globals.py

--- lib/globals.py
+++
@@ -1,6 +0,0 @@
-# Script to define global variables
-FISEVI_URL = 'http://fisevi.com/investigador-ofertas-de-empleo/'
-OUTPUT = 'output'
-CRED_PATH = 'security/credentials.txt'
-SMTP_CLIENT = 'smtp.office365.com'
-RECEIVERS = ['alberto.becerra.tome@gmail.com',]
